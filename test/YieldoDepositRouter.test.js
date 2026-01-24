fedd6f280897bbde7558764991f977e248872a1b
CoolSaki <jayli1254u@gmail.com>
2026-01-24 10:18:10 -0800

    test init
    I built contract test script scratch file.

---
+++ test/YieldoDepositRouter.test.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai")
+const hre = require("hardhat")
+const { ethers } = require("hardhat")
+const { BigNumber } = require("ethers")
+const {
+    expectRevert
+} = require('@openzeppelin/test-helpers');
+
+
+describe("deposit", function () {
+    before(async function () {
+    });
+
+  describe("split fee", async () => {
+    it("should success", async () => {
+    });
+  })
+})
